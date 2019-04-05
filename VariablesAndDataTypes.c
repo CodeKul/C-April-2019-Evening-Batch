fe53c4023007459ad1d9ecbd1a5d014e9807dcc6
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-04-05 19:03:56 +0530

    more on data types

--- VariablesAndDataTypes.c
+++ VariablesAndDataTypes.c
@@ -4,7 +4,28 @@ int main() {
 
     int a = 10;
     float b = 20.30;
-    char c = 'A';
+    char c = 70;
+
+// escape characters
+/*
+    \n new line
+    \b backspace
+    \a audio
+    \r carriage return
+    \t tab
+*/
+
+// Format specifiers
+/*
+    %d - int
+    %f - float
+    %c - char
+*/
+
+    printf("%d\t", a);
+    printf("%f\t",b);
+    printf("%c\n",c);
+
 
     return 0;
 }
